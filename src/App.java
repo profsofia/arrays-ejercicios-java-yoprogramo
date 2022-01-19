96816f3262fa64a9b86b0250b1da43bf3cd2f1cd
Sofía Schenone <sofiainesschenone@gmail.com>
2022-01-19 14:24:41 -0300

    agregamos el método para leer por teclado valores

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        /**Consignas
+         * 1. Se necesita de un vector que sea capaz de almacenar 10 números enteros entre 1 y 100. Realizar un programa que permita la carga por teclado de los 10 números solicitados.
+         */
+        int[] vector_one = new int[9];
+
+        try (Scanner keyBoard = new Scanner(System.in)) {
+            for(int index=0; index<vector_one.length; index++){
+                System.out.println("Ingrese el número para la posición :"+ index);
+                //lee el dato del teclado
+                int key = keyBoard.nextInt();
+                //lo asigna en el indice correspondiente
+                vector_one[index] = key;
+            }
+        }
+
     }
 }
