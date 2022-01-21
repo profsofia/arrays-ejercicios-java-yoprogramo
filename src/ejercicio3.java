426f17ca7f825230eb9a56a72fac1ed7285862f4
Sofía Schenone <sofiainesschenone@gmail.com>
2022-01-21 12:42:46 -0300

    supuestamente terminado el ejercicio 3

--- src/ejercicio3.java
+++ src/ejercicio3.java
@@ -5,20 +5,30 @@ public class ejercicio3 {
     /**Consigna
     * 3.      Se tienen tres vectores. En el primero de ellos se guarda un número de dni, en el segundo un nombre y en el tercero un apellido. Se desea un programa que sea capaz de recorrer los tres vectores AL MISMO TIEMPO y mostrar cada uno de estos datos por pantalla. Pista: tener en cuenta que el índice de cada vector es correspondiente al índice de los demás, es decir, los datos contenidos en el índice cero del vector de dni, se corresponde con el índice cero del vector de nombres y el de apellidos.
     */
-     long obtener_dni[] = new long[4]; 
-     String obtener_nombre[] = new String[4];
-     String obtener_apellido[] = new String[4];
+     long obtener_dni[] = new long[2]; 
+     String obtener_nombre[] = new String[2];
+     String obtener_apellido[] = new String[2];
      try (Scanner dni = new Scanner(System.in)){
-         for(int index_dni=0; index_dni<4; index_dni++){
+         for(int i=0; i<2; i++){
              System.out.println("Ingrese su DNI(sin puntos ni guiones): ");
              //leemos el dato que ingresa el usuario
              long num_dni = dni.nextLong();
              //lo colocamos en una variable
-             obtener_dni[index_dni] = num_dni;
+             obtener_dni[i] = num_dni;
+             //para el nombre.
+             System.out.println("Ingrese su nombre: ");
+             String nombre = dni.nextLine();
+             obtener_nombre[i] = nombre;
+             //para el apellido.
+             System.out.println("Ingrese su apellido: ");
+             String apellido = dni.nextLine();
+             obtener_apellido[i] = apellido;
+             System.out.println(num_dni +" ." +nombre + " ." +apellido);
+
          }
      }
      catch(Exception e) {
-         System.out.println("Error al obtener DNI, intente nuevamente");
+         System.out.println("Error al obtener los datos, intente nuevamente");
      }
 }
 }
---
+++ src/matrices2.java
@@ -0,0 +1,11 @@
+public class matrices2 {
+    public static void main(String[] args){
+        /**
+         * 2.      Una escuela primaria utiliza una matriz para calcular los promedios de sus alumnos. Para ello tienen una matriz de 10 filas, donde cada fila representa a un alumno y 4 columnas. Las primeras 3 columnas representan las notas de cada uno de los alumnos, mientras que la 4 es el promedio de las mismas. Se desea un programa que sea capaz de permitir la carga por teclado de las 3 notas de cada alumno, de realizar el cálculo automático del promedio, de guardar el mismo en la 4 columna y luego mostrar por pantalla cada una de las notas y el promedio obtenido. Por ejemplo.
+
+
+         Nota: tener en cuenta que generalmente las calificaciones y los promedios no suelen ser números enteros, sino que pueden tener decimales.
+         */
+        System.out.println("Hola ejercicio 3");
+    }
+}
