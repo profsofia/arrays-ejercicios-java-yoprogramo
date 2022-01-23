7940cc01eaf9edc63ece1edb2873e8759748dde8
Sofía Schenone <sofiainesschenone@gmail.com>
2022-01-23 10:06:02 -0300

    corregido el ejercicio 3, sin fallas

--- src/ejercicio3.java
+++ src/ejercicio3.java
@@ -5,14 +5,15 @@ public class ejercicio3 {
     /**Consigna
     * 3.      Se tienen tres vectores. En el primero de ellos se guarda un número de dni, en el segundo un nombre y en el tercero un apellido. Se desea un programa que sea capaz de recorrer los tres vectores AL MISMO TIEMPO y mostrar cada uno de estos datos por pantalla. Pista: tener en cuenta que el índice de cada vector es correspondiente al índice de los demás, es decir, los datos contenidos en el índice cero del vector de dni, se corresponde con el índice cero del vector de nombres y el de apellidos.
     */
-     long obtener_dni[] = new long[2]; 
-     String obtener_nombre[] = new String[2];
-     String obtener_apellido[] = new String[2];
+     long obtener_dni[] = new long[1]; 
+     String obtener_nombre[] = new String[1];
+     String obtener_apellido[] = new String[1];
      try (Scanner dni = new Scanner(System.in)){
          for(int i=0; i<2; i++){
              System.out.println("Ingrese su DNI(sin puntos ni guiones): ");
              //leemos el dato que ingresa el usuario
              long num_dni = dni.nextLong();
+             dni.nextLine();
              //lo colocamos en una variable
              obtener_dni[i] = num_dni;
              //para el nombre.
